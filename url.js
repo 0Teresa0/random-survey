c7e707a57b9be4794859c1e54c00a3ee724c407f
0Teresa0 <shink8jsm@gmail.com>
2025-05-21 22:03:27 +0800

    Update url.js

--- url.js
+++ url.js
@@ -7,8 +7,22 @@
  */
 
 
-const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
-];
+ const links = [
+      "https://www.surveycake.com/s/eGpgq",
+      "https://www.surveycake.com/s/NV8qG",
+      "https://www.surveycake.com/s/PZlXy",
+      "https://www.surveycake.com/s/qG1dd",
+      "https://www.surveycake.com/s/WGYKA",
+      "https://www.surveycake.com/s/0ZWPL"
+    ];
+    const selected = links[Math.floor(Math.random() * links.length)];
+    setTimeout(() => {
+      window.location.href = selected;
+    }, 2000); // 2秒後跳轉
+  </script>
+</head>
+<body>
+  <h1>請稍候，系統正在為您分配問卷...</h1>
+  <p>即將自動導向問卷頁面，請不要關閉視窗。</p>
+</body>
+</html>
